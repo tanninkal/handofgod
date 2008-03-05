fe978df35facc90a6cc1d3618861f767db21cba5
tannin <tannin@9676a374-4f47-0410-9af3-ed1811af17d4>
2008-03-05 16:47:51 +0000

    temporary main module, just here for testing

---
+++ trunk/sourcetmp/main.cpp
@@ -0,0 +1,31 @@
+#include "logger.h"
+
+int main_lh; //logger handle
+char main_tbuf[LOGGER_MAXTEXT];
+
+int die(int ret);
+
+int main(int agrc, char** argv)
+{
+	logger_init("main");
+	logger_sl(LS_D);
+	
+	main_lh = logger_regmodule("main");
+	logger_sl(main_lh, LS_D);
+	logger_openhandle(main_lh);
+	
+	log(main_lh, LOGSEV_STATUS, "main up!");
+	
+	return die(0);
+}
+
+int die(int ret)
+{
+	log(main_lh, LS_ST, "beginning main shutdown");
+	lnprintf(main_lh, LS_ST, "exit code: %d", ret);
+	logger_closehandle(main_lh);
+	logger_unreg(main_lh);
+	log(main_lh, LS_ST, "main shutdown complete");
+	logger_exit();
+	return ret;
+}
